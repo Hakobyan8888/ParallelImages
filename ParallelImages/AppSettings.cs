d8b0ca85e7dbc64616fdf3d86a1e8f4d69d2d630
StepanHokobyan <31405336+StepanHokobyan@users.noreply.github.com>
2019-05-10 21:41:34 +0400

    The ParallelImages

---
+++ ParallelImages/AppSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParallelImages
+{
+    class AppSettings
+    {
+        public string FilePathFirst { get; set; } = "./Pictures";
+        public string FilePathSecond { get; set; } = "./Images";
+    }
+}
--- ParallelImages/Program.cs
+++ ParallelImages/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Threading;
 
 namespace ParallelImages
 {
@@ -6,7 +10,21 @@ namespace ParallelImages
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            AppSettings appSettings = new AppSettings();
+            string[] files = Directory.GetFiles(appSettings.FilePathFirst, "*.jpg");
+            string newDir = appSettings.FilePathSecond;
+            Directory.CreateDirectory(newDir);
+
+            Parallel.ForEach(files, (currentFile) =>
+            {
+                string filename = Path.GetFileName(currentFile);
+                Bitmap bitmap = new Bitmap(currentFile);
+                bitmap.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                bitmap.Save(Path.Combine(newDir, filename));
+                
+                Console.WriteLine($"Processing {filename} on thread {Thread.CurrentThread.ManagedThreadId}");
+            });
+
         }
     }
 }
